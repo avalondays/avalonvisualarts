a26b3320f5a79c7acca8e235d1953a1089258427
avalondays <avalondays@users.noreply.github.com>
2024-02-17 11:11:46 -0700

    cleanup

--- scripts.js
+++ scripts.js
@@ -15,3 +15,17 @@ var slider = tns({
 });
 
 document.location.reload(true)
+
+ // Remove the .html extension from the URL
+ function removeHtmlExtension() {
+    var currentUrl = window.location.href;
+    if (currentUrl.substr(-5) === '.html') {
+      var newUrl = currentUrl.substr(0, currentUrl.length - 5);
+      window.history.replaceState({}, document.title, newUrl);
+    }
+  }
+
+  // Call the function when the page loads
+  window.onload = function() {
+    removeHtmlExtension();
+  };
